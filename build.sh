51a10cbdc7eebc3eb5455883f8c987520d1917cb
13dagger <13dagger@gmail.com>
2020-03-29 18:18:19 +0300

    initial db dump, small fixes

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 source .env || exit
 
-rm nginx/*.conf -r
+rm nginx/*.conf -rf
 cp src/nginx/*.conf nginx/
 sed -i "s/\[DOMAIN]/$DOMAIN/" nginx/*.conf
 
 cp .env app/.env
 
+docker-compose down
 docker-compose up -d --build
